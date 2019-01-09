39e9486d7112d5cf62ad24a412148e7082a31660
sogyal <sogyals429@gmail.com>
2019-01-09 16:51:20 +1100

    Add search stock ajax and errors

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class StocksController < ApplicationController
+  def search
+    if params[:stock].blank?
+      flash.now[:danger] = 'You have entered an empty search string'
+    else
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = 'You have entered an incorrect stock symbol' unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
+    end
+  end
+end
